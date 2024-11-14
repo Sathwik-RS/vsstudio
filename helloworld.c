4035c07ac9acbbda30c99b1cf8afe5bccd734015
Sathwik-RS <rssathwik5@gmail.com>
2024-11-14 10:20:13 +0530

    message

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+    #include<stdlib.h>
+        void main()
+            {
+                int num1,num2;
+           printf("enter two numbers");
+         scanf("%d%d",&num1,&num2);
+              num1=num1+num2;
+         num2=num1-num2;
+                      num1=num1-num2;
+         printf(" two numbers %d\n%d",num1,num2);
+}
